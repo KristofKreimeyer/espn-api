717454510d863703f6fde06dbf8a49fb0f955235
Kristof Kreimeyer <kontakt@kristof-kreimeyer.de>
2020-08-21 14:25:40 +0200

    Rebuild ESPN API

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import cheerio from 'cheerio';
+import request from "request";
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      nba: {}
+    }
+  },
+  methods: {
+    fetchNBAData() {
+      request('https://www.espn.com/nba/scoreboard', (error,response,html) => {
+        if(!error && response.statusCode === 200) {
+          const $ = cheerio.load(html);
+          const scripts = $("script").toArray();
+          const scoreboard = scripts.find(script =>
+              script.children[0] &&
+              script.children[0].data.includes("window.espn.scoreboardData"))
+              .children[0].data;
+
+          const strippedData = scoreboard
+              .replace("window.espn.scoreboardData", "")
+              .replace("=", "")
+              .replace("if(!window.espn_ui.device.isMobile){window.espn.loadType = \"ready\"};", "")
+              .replace(/;/g, "").split("window.espn.scoreboardSettings")[0].trim();
+
+          const data = JSON.parse(strippedData);
+
+          this.nba = data;
+          console.log(this.nba);
+        }
+      })
+    }
+  },
+  mounted() {
+    this.fetchNBAData();
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
